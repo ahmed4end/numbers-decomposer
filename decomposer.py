d9a0306948b2c240aa93d1040bf2777f487332fe
ahmed4end <35382418+ahmed4end@users.noreply.github.com>
2020-03-26 23:43:22 +0200

    Add files via upload

---
+++ decomposer.py
@@ -0,0 +1,50 @@
+from random import randint, shuffle
+
+class decompose():
+	def __init__(self, n, auto=True):
+		first_layer = self.decomposeS(n)
+		res = []
+		for i in first_layer:
+			m = self.decomposeM(i)
+			if m!= None:res.append("+".join(str(j)+"^2" for j in m))
+			elif randint(1,10)%2==0:
+				formated =  "-".join(str(j) for j in self.decomposeT(i))
+				res.append(formated if len(formated)!=2 else formated[:-1])
+			else:res.append(str(i))
+		print("+".join(res))
+	def decomposeM(self, n):
+	    def rec(n, i):
+	        if n<0:return None
+	        if n==0:return []
+	        for j in range(i-1, 0, -1):
+	            hold = rec(n-j**2, j)
+	            if hold!=None:return [j] + hold
+	    return rec(n, int(n**0.5))
+
+	def decomposeS(self, n):
+	    rand = list(range(1, n))
+	    shuffle(rand)
+	    def rec(n):
+	        if n<0:return None
+	        if n==0:return []
+	        for j in rand:
+	            shuffle(rand)     
+	            hold = rec(n-j)
+	            if hold!=None:return [j] + hold
+	    return rec(n)
+
+
+	def decomposeT(self, n):
+	    rand = list(range(1, n))
+	    random_start = randint(n, n+int(n*0.5))
+	    def rec(n2, r):
+	        if n2<0:return None
+	        if n2==n:return []
+	        r = [i for i in r if (n2-i)>=n]
+	        r = sorted(r)[::-1]
+	        for j in r:
+	            hold = rec(n2-j, r)
+	            if hold!=None:return [j] + hold
+	    return [random_start] + rec(random_start, rand)
+
+decompose(100)
